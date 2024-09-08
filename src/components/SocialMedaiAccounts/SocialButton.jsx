38b01eced520780ee6f9cdcfc6b1306ae1d830d6
Naveen Saini <naveensaini94667@gmail.com>
2024-09-08 15:54:17 +0530

    init

--- src/components/SocialMedaiAccounts/SocialButton.jsx
+++ src/components/SocialMedaiAccounts/SocialButton.jsx
@@ -1,11 +1,12 @@
-const SocialButton = ({ platform, color, icon }) => (
+const SocialButton = ({ platform, color, icon ,link}) => (
     <a
-      href="#"
-      className={` z-1000 relative flex items-center w-50 px-4 py-1 rounded-l-full ${color} hover:right-0 transition-all duration-500 ease-out -right-[180px]  group`}
+      href={link}
+      className={`relative flex items-center w-auto md:w-auto px-4 py-1 rounded-l-full ${color} hover:right-0 transition-all duration-500 ease-out hover:scale-110  group`}
+    target="_blank"
     >
       <div className="flex items-center">
         {icon}
-        <p className="ml-2 text-white font-lato text-xl">{platform}</p>
+        <p className="ml-2 text-white font-lato text-sm md:text-xl">{platform}</p>
       </div>
     </a>
   );
