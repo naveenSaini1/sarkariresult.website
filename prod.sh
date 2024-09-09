b9b52eca25b2d1e07b1eee4eae70254b5d38af92
naveen saini <naveensaini94667@gmail.com>
2024-09-09 17:00:25 +0000

    init

--- prod.sh
+++ prod.sh
@@ -6,12 +6,12 @@ git config --global user.email "naveensaini94667@gmail.com"
 # Navigate to the repository directory
 cd  /projects/sarkariresult.website
 
-git remote add origin https://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@github.com/naveenSaini1/sarkariresult.website.git
+git pull origin master
 
-# Stage changes
 git add .
 
 # Commit changes
 
 git commit -m "init"
 
+git push origin master
