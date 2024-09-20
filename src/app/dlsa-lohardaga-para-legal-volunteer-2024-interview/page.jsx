00e52f5e2e56fe6e593ec6b4820efb3e9109cb2a
naveen saini <naveensaini94667@gmail.com>
2024-09-20 11:02:12 +0000

    init

---
+++ src/app/dlsa-lohardaga-para-legal-volunteer-2024-interview/page.jsx
@@ -0,0 +1,198 @@
+export default function RecruitmentInformation() {
+  // Displays recruitment information for DLSA, Lohardaga Para Legal Volunteer 2024
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">
+        DLSA, Lohardaga Para Legal Volunteer 2024
+      </h1>
+      <p className="text-gray-600 mb-4">
+        <span className="font-bold text-green-500">Name of the Post:</span>{" "}
+        DLSA, Lohardaga Para Legal Volunteer 2024 Interview Date & List
+        Released
+      </p>
+      <p className="text-gray-600 mb-4">
+        <span className="font-bold text-green-500">Post Date:</span>{" "}
+        <span className="text-gray-800">10-09-2024</span>
+      </p>
+      <p className="text-gray-600 mb-4">
+        <span className="font-bold text-green-500">Latest Update:</span>{" "}
+        20-09-2024
+      </p>
+      <p className="text-gray-600 mb-4">
+        <span className="font-bold text-green-500">Total Vacancy:</span> 39
+      </p>
+      <p className="text-gray-600 mb-4">
+        <span className="font-bold text-red-500">Brief Information:</span>{" "}
+        District Legal Service Authority (DLSA), Lohardaga has given a
+        notification for the recruitment of Para Legal Volunteer Vacancy. Those
+        Candidates who are interested in the vacancy details & completed all
+        eligibility criteria can read the Notification & Apply.
+      </p>
+      <div className="mb-4">
+        {/*  <div data-fuse="23019200358"></div> */}
+      </div>
+      <table className="table-auto w-full border-collapse border border-gray-300 mb-4">
+        <tbody>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-red-500">
+                District Legal Service Authority (DLSA), Lohardaga
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-green-500">
+                Para Legal Volunteer Vacancy 2024
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <a
+                href="https://www.freejobalert.com"
+                target="_blank"
+                rel="noopener"
+                className="underline text-blue-500"
+              >
+                
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-purple-500">
+                {/* <a href="https://play.google.com/store/apps/details?id=com.freejobalert" target="_blank" rel="nofollow"> */}
+                Download Mobile App
+                {/* </a> */}
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-red-500">Important Dates</span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="p-2">
+              <ul className="list-disc ml-6">
+                <li>
+                  Last Date to Apply Receipt of Application:{" "}
+                  <strong>17-09-2024</strong>
+                </li>
+                <li>Date of Interview: <strong>20 to 22-09-2024</strong></li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-red-500">Qualification</span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="p-2">
+              <ul className="list-disc ml-6">
+                <li>Candidates Should Possess 10th Class.</li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-red-500">Vacancy Details</span>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-center p-2">
+              <span className="font-bold text-purple-500">Post Name</span>
+            </td>
+            <td className="text-center p-2">
+              <span className="font-bold text-purple-500">Total</span>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-center p-2">Para Legal Volunteer</td>
+            <td className="text-center p-2">39</td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-blue-500">
+                Interested Candidates Can Read the Full Notification Before Apply
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-center p-2">
+              <span className="font-bold text-red-500">Important Links</span>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-center p-2">
+              <span className="font-bold text-green-500">
+                Interview Date & List (20-09-2024)
+              </span>
+            </td>
+            <td className="text-center p-2">
+              <a
+                href="https://img.freejobalert.com/uploads/2024/09/Interview-Date-List-DLSA-Lohardaga-Para-Legal-Volunteer-Posts.pdf"
+                target="_blank"
+                rel="nofollow"
+                className="underline text-blue-500"
+              >
+                Click Here
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-center p-2">
+              <span className="font-bold text-green-500">Notification</span>
+            </td>
+            <td className="text-center p-2">
+              <a
+                title="DLSA, Lohardaga"
+                href="https://img.freejobalert.com/uploads/2024/09/Notification-DLSA-Lohardaga-PLV-Posts.pdf"
+                target="_blank"
+                rel="nofollow"
+                className="underline text-blue-500"
+              >
+                Click Here
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-center p-2">
+              <span className="font-bold text-green-500">Official Website</span>
+            </td>
+            <td className="text-center p-2">
+              <a
+                title="DLSA, Lohardaga"
+                href="https://lohardaga.dcourts.gov.in/"
+                target="_blank"
+                rel="nofollow"
+                className="underline text-blue-500"
+              >
+                Click Here
+              </a>
+            </td>
+          </tr>
+          {/*  <tr>
+            <td className="text-center p-2">
+              <span className="font-bold text-purple-500">
+                Download Mobile App
+              </span>
+            </td>
+            <td className="text-center p-2">
+              <a
+                className="color: #0000ff;"
+                href="https://play.google.com/store/apps/details?id=com.freejobalert"
+                target="_blank"
+                rel="nofollow"
+              >
+                Click Here
+              </a>
+            </td>
+          </tr> */}
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/app/jkssb-patwari-02-2021-omr-exam-final-answer-key-re/page.jsx
@@ -0,0 +1,324 @@
+export default function JKSSBRecruitment() {
+  // Displays recruitment information for JKSSB Patwari (02 of 2021)
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">JKSSB Patwari (02 of 2021) OMR Based Written Exam Final Answer Key Released</h1>
+      <div className="mb-4">
+        <p className="text-lg font-medium mb-2">
+          <span className="text-green-500">Name of the Post:</span> JKSSB Patwari (02 of 2021) OMR Based Written Exam Final Answer Key Released
+        </p>
+        <p className="text-lg font-medium mb-2">
+          <span className="text-green-500">Post Date: </span>
+          <span className="text-black">29-03-2021</span>
+        </p>
+        <p className="text-lg font-medium mb-2">
+          <span className="text-green-500">Latest Update:</span> 20-09-2024
+        </p>
+        <p className="text-lg font-medium mb-2">
+          <span className="text-green-500">Total Vacancy: </span>
+          <span className="text-black">2311</span>
+        </p>
+      </div>
+      <p className="text-lg mb-4">
+        <span className="text-red-500 font-medium">Brief Information:</span> J&K Services Selection Board (JKSSB) has announced a notification for the recruitment of Junior Asst, Steno, Driver & Other vacancies in Various Departments. Those Candidates who are interested in the vacancy details & completed all eligibility criteria can read the Notification & Apply Online.
+      </p>
+
+      <div className="mb-4">
+        <h2 className="text-2xl font-bold mb-2">J&K Services Selection Board (JKSSB)</h2>
+        <p className="text-lg font-medium mb-2 text-green-500">Various Vacancy 2021</p>
+        <p className="text-lg font-medium mb-2 text-pink-500">Advt No. 02 of 2021</p>
+        <p className="text-lg font-medium mb-2">
+          <a href="https://www.freejobalert.com" target="_blank" rel="noopener" className="text-blue-500 hover:underline"></a>
+        </p>
+      </div>
+
+      <div className="mb-4">
+        <h3 className="text-xl font-bold mb-2 text-red-500">Application Fee</h3>
+        <ul className="list-disc pl-5">
+          <li className="text-lg mb-2">Application Fee: <strong>Rs. 350/-</strong></li>
+          <li className="text-lg mb-2">Payment Mode: <strong>only online through Net Banking, by using Credit or Debit cards.</strong></li>
+        </ul>
+      </div>
+
+      <div className="mb-4">
+        <h3 className="text-xl font-bold mb-2 text-red-500">Important Dates</h3>
+        <ul className="list-disc pl-5">
+          <li className="text-lg mb-2">Starting date to apply online: <strong>16-04-2021 (12-04-2021 Changed to 16-04-2021)</strong></li>
+          <li className="text-lg mb-2">Last Date to apply online: <strong>10-06-2021</strong> <span className="text-red-500">(Again Extended from 20-05-2021 to 10-06-2021)</span> (Extended from <span className="text-black">12-05-2021 to 20-05-2021)</span></li>
+        </ul>
+        <p className="text-lg font-medium mb-2">Dates for Examination:</p>
+        <ul className="list-disc pl-5">
+          <li className="text-lg mb-2">CBT Dates for Health & Medical Education Dept: <strong>17 to 24–08-2021</strong></li>
+          <li className="text-lg mb-2">Reschedule Exam Date for Candidates who could not take CBT on 18-08-2021: <strong>25-08-2021</strong></li>
+          <li className="text-lg mb-2">Dates for Downloading Admit Card: <strong>07-08-2021</strong></li>
+          <li className="text-lg mb-2">Last Date for Submission of Answer key objections: <strong>29-08-2021</strong></li>
+          <li className="text-lg mb-2">Date for Edit Option: <strong>17 to 21-06-2021</strong></li>
+          <li className="text-lg mb-2">CBT Dates for Library Assistant (Health & Medical Education): <strong>26-09-2021 (Postponed)</strong></li>
+          <li className="text-lg mb-2">Short Hand/ Skill Test Dates for Steno Typist, Stenographer, Jr Asst, Clerk: <strong>From 11-10-2021 to 18-10-2021</strong></li>
+          <li className="text-lg mb-2">Date of CBT Exam (Phase II): <strong>08 to 12-11-2021</strong></li>
+          <li className="text-lg mb-2">Dates for Driver Driving Test: <strong>04-10-2021 to 31-12-2021</strong></li>
+          <li className="text-lg mb-2">DV Dates for ED: 17 to 25-08-2021 Candidates: <strong>From 20-11 to 04-12-2021</strong></li>
+          <li className="text-lg mb-2">Dates for Computer Based Type Test (Skill Test) Phase II: <strong>From 10-11-2021 to 10-12-2021</strong></li>
+          <li className="text-lg mb-2">Dates for 3rd Phase Skill Test: <strong>25, 26, 29 & 30-11-2021</strong></li>
+          <li className="text-lg mb-2">Dates for 4th Phase Skill Test: <strong>01 to 31-12-2021</strong></li>
+          <li className="text-lg mb-2">DV Dates for ED: 17 to 25-08-2021 Candidates: <strong>06 & 07-12-2021</strong></li>
+          <li className="text-lg mb-2">Date of CBT: <strong>29-11-2022</strong></li>
+          <li className="text-lg mb-2">Date of DV: <strong>20-11-2023</strong></li>
+          <li className="text-lg mb-2">Date of Written Exam for Patwari: <strong>31-03-2024</strong></li>
+          <li className="text-lg mb-2">Date of Type Test & Short Hand Test: <strong>03-07-2024</strong></li>
+          <li className="text-lg mb-2">Date of OMR Based Written Exam Date: <strong>01-09-2024</strong></li>
+        </ul>
+      </div>
+
+      <div className="mb-4">
+        <h3 className="text-xl font-bold mb-2 text-red-500">Age Limit (as on 01-01-2020)</h3>
+        <ul className="list-disc pl-5">
+          <li className="text-lg mb-2">For OM: <strong>40 Years</strong></li>
+          <li className="text-lg mb-2">For SC/ ST/ RBA/ ALC/ IB/ EWS/ PSP/ OSC: <strong>43 Years</strong></li>
+          <li className="text-lg mb-2">For Physically Challenged Person: <strong>42 Years</strong></li>
+          <li className="text-lg mb-2">For Ex-Servicemen: <strong>48 Years</strong></li>
+          <li className="text-lg mb-2">For Government Service/Contractual Employment: <strong>40 Years</strong></li>
+        </ul>
+      </div>
+
+      <h2 className="text-2xl font-bold mb-4 text-red-500">Vacancy Details</h2>
+      <table className="table-auto w-full text-center border-collapse border-2 border-gray-300">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="px-4 py-2 text-pink-500 font-medium">Post Name</th>
+            <th className="px-4 py-2 text-pink-500 font-medium">Total</th>
+            <th className="px-4 py-2 text-pink-500 font-medium">Qualification</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Health & Medical Education Dept */}
+          <tr className="bg-gray-200">
+            <td className="px-4 py-2" colSpan="3">
+              <span className="text-black font-medium">Health & Medical Education Dept</span>
+            </td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Artist</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">10+2, Diploma/ Degree (Fine Arts)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Occupational<br/> Therapist</td>
+            <td className="px-4 py-2">04</td>
+            <td className="px-4 py-2">10+2, Diploma/ Degree (Occupational Therapy)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Laboratory<br/> Technician</td>
+            <td className="px-4 py-2">11</td>
+            <td className="px-4 py-2">10+2, DMLT/ BMLT</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">E.C.G<br/> Technician</td>
+            <td className="px-4 py-2">10</td>
+            <td className="px-4 py-2">10+2, Diploma (ECG)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Physiotherapist</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">10+2, Diploma/Degree (Physiotherapy)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">X- Ray Technician</td>
+            <td className="px-4 py-2">07</td>
+            <td className="px-4 py-2">10+2, Diploma(X-ray Asst)/Degree (X-ray Technology/Radiography)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Radiotherapy<br/> Technician</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">10+2, Diploma/Degree (Radiation Therapy Technology)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Stenographer</td>
+            <td className="px-4 py-2">05</td>
+            <td className="px-4 py-2" rowSpan="2">Any Degree,Typing knowledge</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Steno Typist</td>
+            <td className="px-4 py-2">11</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Theatre<br/> Assistant</td>
+            <td className="px-4 py-2">05</td>
+            <td className="px-4 py-2">10+2, Diploma/ Degree (Operation Theatre<br/> Technology)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Paramedical<br/> Assistant(Der<br/> matology)</td>
+            <td className="px-4 py-2">01</td>
+            <td className="px-4 py-2">10+2, Diploma (Leprosy)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior Staff<br/> Nurse/Junior<br/> Grade Nurse</td>
+            <td className="px-4 py-2">350</td>
+            <td className="px-4 py-2">GNM, B.Sc. (Nursing)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Carpenter</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">10+2, ITI</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">X<br/> -Ray<br/> Assistant</td>
+            <td className="px-4 py-2">36</td>
+            <td className="px-4 py-2">10+2, Diploma(X-ray Asst)/Degree (X-ray Technology/Radiography)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Telephone<br/> Operator</td>
+            <td className="px-4 py-2">02</td>
+            <td className="px-4 py-2">10+2, Diploma/Degree (Electronics &<br/> Communication)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Senior<br/> Laboratory<br/> Assistant</td>
+            <td className="px-4 py-2">10</td>
+            <td className="px-4 py-2">10+2, Diploma (Medical<br/> Assistant Course) degree(Laborotary Technology Course)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Library<br/> Assistant</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">Degree (Library Science)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Medical<br/> Record<br/> Technician</td>
+            <td className="px-4 py-2">12</td>
+            <td className="px-4 py-2">10+2, Diploma (Medical<br/> Record Technician)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Pharmacist/<br/> Medical<br/> Assistant</td>
+            <td className="px-4 py-2">32</td>
+            <td className="px-4 py-2">10+2, Medical<br/> Assistant/Pharmicist<br/> training</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Assistant</td>
+            <td className="px-4 py-2">96</td>
+            <td className="px-4 py-2">Any Degree,Typing knowledge</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Driver–II</td>
+            <td className="px-4 py-2">10</td>
+            <td className="px-4 py-2">Matric & 10+2, Valid Driving Licence</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Receptionist</td>
+            <td className="px-4 py-2">05</td>
+            <td className="px-4 py-2">10+2, Degree/Diploma (Hotel Management)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Driver-I</td>
+            <td className="px-4 py-2">07</td>
+            <td className="px-4 py-2">Matric & 10+2, Valid Driving Licence</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior Store<br/> Keeper</td>
+            <td className="px-4 py-2">10</td>
+            <td className="px-4 py-2">Any Degree</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Injector</td>
+            <td className="px-4 py-2">02</td>
+            <td className="px-4 py-2">10+2, DMLT/ BMLT</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Tailor</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">Matric & 10+2, ITI</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Liftman/Lift<br/> operator</td>
+            <td className="px-4 py-2">05</td>
+            <td className="px-4 py-2">Matric & 10+2, ITI</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">FMPHW/AN<br/> M</td>
+            <td className="px-4 py-2">08</td>
+            <td className="px-4 py-2">Diploma (FMPHW/MMPHW/AN<br/> M)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Barber</td>
+            <td className="px-4 py-2">02</td>
+            <td className="px-4 py-2">Matric & 10+2</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior Plumber</td>
+            <td className="px-4 py-2">13</td>
+            <td className="px-4 py-2" rowSpan="3">Matric & 10+2, ITI</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Electrician</td>
+            <td className="px-4 py-2">05</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Gas Pipe Line<br/> operator</td>
+            <td className="px-4 py-2">05</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Museum<br/> Curator</td>
+            <td className="px-4 py-2">01</td>
+            <td className="px-4 py-2">B.Sc/ M.Sc (Zoology)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Occupational<br/> Therapist</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">10+2, Diploma/ Degree (Occupational Therapy)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Senior<br/> Radiotherpary<br/> Tech</td>
+            <td className="px-4 py-2">04</td>
+            <td className="px-4 py-2" rowSpan="2">10+2, Diploma (Medical<br/> Assistant Course) degree(Laborotary Technology Course)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Laboratory<br/> Technologist</td>
+            <td className="px-4 py-2">02</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Speech<br/> Therapist</td>
+            <td className="px-4 py-2">01</td>
+            <td className="px-4 py-2">10+2, Diploma/Degree (Speech Therapy)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Tutor/Clinical<br/> Instructor /<br/> Public Health<br/> Tutor</td>
+            <td className="px-4 py-2">06</td>
+            <td className="px-4 py-2">B. Sc</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Psychiatric<br/> Social Worker</td>
+            <td className="px-4 py-2">01</td>
+            <td className="px-4 py-2" rowSpan="2">Degree/ MA (Psychology/Sociology/ MSW</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Social<br/> Worker/Medica<br/> l Social Worker</td>
+            <td className="px-4 py-2">08</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Junior<br/> Stenographer</td>
+            <td className="px-4 py-2">01</td>
+            <td className="px-4 py-2">Any Degree,Typing knowledge</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Medical<br/> Physics<br/> Technician</td>
+            <td className="px-4 py-2">02</td>
+            <td className="px-4 py-2">Diploma/Degree (Radiological/Medical<br/> Physics) M.Sc. (Physics)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Clinical<br/> Psychologist</td>
+            <td className="px-4 py-2">03</td>
+            <td className="px-4 py-2">Masters Degree/ M. Phil (Psychology)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Remedial<br/> Therapist</td>
+            <td className="px-4 py-2">02</td>
+            <td className="px-4 py-2">B. Ed Special Education<br/> (Remedial)</td>
+          </tr>
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-2">Rehabilitation<br/> Psychologist</td>
+            <td className="px-4 py-2">01</td>
+            <td className="px-4 py-2">Degree/ MA (Psychology/Sociology/ MSW</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
