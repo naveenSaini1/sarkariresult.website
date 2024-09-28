510b46612add5e16e6d5a9596825934f2b03b351
Naveen Saini <naveensaini94667@gmail.com>
2024-09-28 15:19:37 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'header',
+                        key: 'host',
+
+                        value: 'www.sarkariresult.website', // Redirect from www
+                    },
+                ],
+                destination: 'http://sarkariresult.website/:path*', // Redirect to non-www
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
