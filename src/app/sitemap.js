3aadb28a61b94089499bf9ff7980f7036b232adb
Naveen Saini <naveensaini94667@gmail.com>
2024-10-04 11:21:14 +0530

    init

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,32 +3,67 @@ import { API_HOST_ADDRESS, endPoints, PUBLIC_PREFIX } from "@/constant/ServerUrl
 import fetchApi from "@/util/fetch";
 import fetchApiNoCache from "@/util/fetchNoCache";
 
-export default async function SiteMap (){
+export default async function SiteMap() {
     const data = await fetchApiNoCache(API_HOST_ADDRESS + PUBLIC_PREFIX + endPoints.getThePostForSiteMap, "GET") || [];
-   console.log(data)
-    const allPost=data?.map((el)=>{
+    console.log(data)
+    const allPost = data?.map((el) => {
         return (
             {
-                url:`${BASE_URL}/${el?.url}`,
-                lastModified:el.timestamp
-                
+                url: `${BASE_URL}/${el?.url}`,
+                lastModified: el.timestamp,
+                changeFrequency: "daily",
+                priority: 0.8,
+
             }
         )
     })
 
     return (
         [
-            {url:BASE_URL,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.about,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.admitCard,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.answerKey,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.contact,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.privacy,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.result,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.search,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.syllabus,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.viewAll,lastModified:new Date()},
-            {url:BASE_URL+clientEndPoints.dailyupdates,lastModified:new Date()},
+            {
+                url: BASE_URL, lastModified: new Date(), changeFrequency: "daily",
+                priority: 1,
+            },
+            {
+                url: BASE_URL + clientEndPoints.about, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.admitCard, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.answerKey, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.contact, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.privacy, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.result, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.search, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.syllabus, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.viewAll, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
+            {
+                url: BASE_URL + clientEndPoints.dailyupdates, lastModified: new Date(), changeFrequency: "daily",
+                priority: 0.9,
+            },
             ...allPost
         ]
     )
