c3870417a5e063414338cee6a537bf36d7cabd72
naveen saini <naveensaini94667@gmail.com>
2024-10-09 05:01:12 +0000

    init

---
+++ src/app/nfl-non-executive-careers-join-team-2024/page.jsx
@@ -0,0 +1,247 @@
+export default function NFLRecruitment() {
+  // This component displays recruitment information for the National Fertilizers Limited (NFL) for the year 2024.
+  // It includes details like vacancy information, important dates, application fees, and eligibility criteria.
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <nav aria-label="breadcrumbs" className="rank-math-breadcrumb">
+        <p>
+          <a href="https://pmsuryaghar.org.in" className="text-blue-500 hover:underline">
+            
+          </a>
+          <span className="separator"> » </span>
+          <a href="https://pmsuryaghar.org.in/category/notification/" className="text-blue-500 hover:underline">
+            Notification
+          </a>
+          <span className="separator"> » </span>
+          <span className="last">
+            NFL Hiring 2024: 336 Non-Executive Roles Available, Apply Online
+          </span>
+        </p>
+      </nav>
+      <header className="entry-header">
+        <h1 className="entry-title font-bold text-3xl mb-4">
+          NFL Hiring 2024: 336 Non-Executive Roles Available, Apply Online
+        </h1>
+        <div className="entry-meta text-gray-600">
+          <span className="posted-on">
+            <time
+              className="entry-date published"
+              datetime="2024-10-09T10:09:16+05:30"
+            >
+              October 9, 2024
+            </time>
+          </span>
+          <span className="byline">
+            by{' '}
+            <span className="author vcard" itemprop="author" itemtype="https://schema.org/Person" itemscope>
+              <a
+                className="url fn n text-blue-500 hover:underline"
+                href="https://pmsuryaghar.org.in/author/pmsuryaghar/"
+                title="View all posts by Admin"
+                rel="author"
+                itemprop="url"
+              >
+                <span className="author-name" itemprop="name">
+                  Admin
+                </span>
+              </a>
+            </span>
+          </span>
+        </div>
+      </header>
+      <div className="entry-content" itemprop="text">
+        <p className="mb-4">
+          <strong>
+            <span className="underline">NFL Non-Executive Hiring 2024</span>
+          </strong>
+          : The National Fertilizers Limited (NFL) has announced the recruitment
+          of 336 Non-Executive roles through Advertisement Number 05
+          (NFL)/ 2024. The NFL Non-Executive Hiring 2024 Notification was
+          released on October 9, 2024, and online applications are open from
+          October 9 to November 8, 2024. Interested and eligible candidates can
+          apply online through the website careers.nfl.co.in or
+          nationalfertilizers.com for various positions including Junior Engineer,
+          Store Assistant, Nurse, Pharmacist, Lab Technician, Account Assistant
+          Attendant Grade-I, etc.
+        </p>
+        <p className="mb-4">
+          Also Read:
+          <a
+            href="https://pmsuryaghar.org.in/territorial-army-bharti-rally/"
+            className="text-blue-500 hover:underline"
+          >
+            <strong>Territorial Army Hiring 2024</strong>
+          </a>
+        </p>
+        <h2 className="wp-block-heading font-bold text-xl mb-4">
+          NFL Hiring 2024 Important Dates
+        </h2>
+        <figure className="wp-block-table">
+          <table className="table-auto w-full text-left">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 font-bold">Event</th>
+                <th className="px-4 py-2 font-bold">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="px-4 py-2">Application Start Date</td>
+                <td className="px-4 py-2">October 9, 2024</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2">Application Last Date</td>
+                <td className="px-4 py-2">November 8, 2024</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2">Exam Date</td>
+                <td className="px-4 py-2">To be Announced</td>
+              </tr>
+            </tbody>
+          </table>
+        </figure>
+        <h2 className="wp-block-heading font-bold text-xl mb-4">
+          NFL Hiring 2024 Application Fees
+        </h2>
+        <figure className="wp-block-table">
+          <table className="table-auto w-full text-left">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 font-bold">Category</th>
+                <th className="px-4 py-2 font-bold">Fees</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="px-4 py-2">General, OBC, EWS</td>
+                <td className="px-4 py-2">Rs. 700/-</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2">SC, ST, PWD, ESM, Deptt.</td>
+                <td className="px-4 py-2">Rs. 150/-</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2">Mode of Payment</td>
+                <td className="px-4 py-2">Online</td>
+              </tr>
+            </tbody>
+          </table>
+        </figure>
+        <h2 className="wp-block-heading font-bold text-xl mb-4">
+          NFL Hiring 2024 Vacancies, Eligibility
+        </h2>
+        <p className="mb-4">
+          <strong>
+            <span className="underline">Age Limit</span>
+          </strong>
+          : The age limit to apply for NFL Non-Executive Hiring 2024 is{' '}
+          <strong>
+            <span className="underline">18-30 Years</span>
+          </strong>
+          . The cutoff date for calculating the age limit is September 30, 2024.
+          Age relaxation will be provided as per the rules.
+        </p>
+        <figure className="wp-block-table">
+          <table className="table-auto w-full text-left">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 font-bold">Post Name</th>
+                <th className="px-4 py-2 font-bold">Vacancy</th>
+                <th className="px-4 py-2 font-bold">Qualification</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="px-4 py-2">Non-Executive Various Roles</td>
+                <td className="px-4 py-2">336</td>
+                <td className="px-4 py-2">Check Notification</td>
+              </tr>
+            </tbody>
+          </table>
+        </figure>
+        <h2 className="wp-block-heading font-bold text-xl mb-4">
+          NFL Non-Executive Hiring 2024 Selection Process
+        </h2>
+        <p className="mb-4">
+          The selection process for NFL Non-Executive Hiring 2024 includes the
+          following stages:
+        </p>
+        <ol className="list-decimal list-inside mb-4">
+          <li>Written Examination</li>
+          <li>Document Verification</li>
+          <li>Medical Examination</li>
+        </ol>
+        <h2 className="wp-block-heading font-bold text-xl mb-4">
+          NFL Non-Executive Hiring 2024 Notification and Online Form Link
+        </h2>
+        <figure className="wp-block-table">
+          <table className="table-auto w-full text-left">
+            <tbody>
+              <tr>
+                <td className="px-4 py-2">
+                  NFL Non-Executive Hiring 2024{' '}
+                  <strong>Short Notice</strong>
+                </td>
+                <td className="px-4 py-2">
+                  <a
+                    href="https://pmsuryaghar.org.in/wp-content/uploads/2024/10/NFL-Non-Executive-Recruitment-2024-Short-Notice.pdf"
+                    className="text-blue-500 hover:underline"
+                  >
+                    <strong>Notice</strong>
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2">
+                  NFL Non-Executive Hiring 2024{' '}
+                  <strong>Notification (Soon)</strong>
+                </td>
+                <td className="px-4 py-2">
+                  <a
+                    href="https://whatsapp.com/channel/0029Va9UXdXFsn0k0mc9UV2w"
+                    target="_blank"
+                    rel="noopener"
+                    className="text-blue-500 hover:underline"
+                  >
+                    <strong>Notification</strong>
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2">
+                  NFL Non-Executive Hiring 2024{' '}
+                  <strong>Online Form (From 9.10.2024)</strong>
+                </td>
+                <td className="px-4 py-2">
+                  <a
+                    href="https://careers.nfl.co.in/"
+                    target="_blank"
+                    rel="noopener"
+                    className="text-blue-500 hover:underline"
+                  >
+                    <strong>Apply Online</strong>
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2">
+                  <strong>Official Website</strong>
+                </td>
+                <td className="px-4 py-2">
+                  <a
+                    href="https://www.nationalfertilizers.com/career/"
+                    target="_blank"
+                    rel="noopener"
+                    className="text-blue-500 hover:underline"
+                  >
+                    <strong>NFL</strong>
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </figure>
+      </div>
+    </div>
+  );
+}
