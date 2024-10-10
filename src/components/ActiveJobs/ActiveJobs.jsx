29b3d5a871086ef5af9afc2e441f31cd3bc03327
Naveen Saini <naveensaini94667@gmail.com>
2024-10-10 17:21:09 +0530

    init

--- src/components/ActiveJobs/ActiveJobs.jsx
+++ src/components/ActiveJobs/ActiveJobs.jsx
@@ -2,10 +2,11 @@ import { getContrastColor, getRandomColor } from "@/util/RandomColor";
 import Jobs from "./Jobs";
 import fetchApi from "@/util/fetch";
 import { API_HOST_ADDRESS, endPoints, PUBLIC_PREFIX } from "@/constant/ServerUrl";
+import fetchApiNoCache from "@/util/fetchNoCache";
 
 
 const ActiveJobs= async()=>{
-    let data=await fetchApi(API_HOST_ADDRESS+PUBLIC_PREFIX+endPoints.getTheBulkPost,"GET") || [];
+    let data=await fetchApiNoCache(API_HOST_ADDRESS+PUBLIC_PREFIX+endPoints.getTheBulkPost,"GET") || [];
     return (
         <div className="mt-10 w-[80%] m-auto">
         {/* <h3 className="mb-4 text-3xl bg-secondaryBgColor text-primaryTxtColor px-2  rounded-xl inline-block">Active Jobs</h3> */}
