2409ef00e314da9c181f1ffbae279ac7ba9eca6d
Naveen Saini <naveensaini94667@gmail.com>
2024-10-13 11:36:39 +0530

    init

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -5,6 +5,7 @@ import fetchApiNoCache from "@/util/fetchNoCache";
 
 export default async function SiteMap() {
     const data = await fetchApiNoCache(API_HOST_ADDRESS + PUBLIC_PREFIX + endPoints.getThePostForSiteMap, "GET") || [];
+    data= data.reverse()
     let centerPost = await fetchApi(API_HOST_ADDRESS + PUBLIC_PREFIX + endPoints.getTheCenters, "GET") || [];
     let statePost = await fetchApi(API_HOST_ADDRESS + PUBLIC_PREFIX + endPoints.getTheStates, "GET") || [];
     statePost   =   statePost?.map((el)=>{
